793f9075ad63b54195c8fc04d32c8b64cb7d15d8
Fuhaid-Ur-Rehman <166975694+Fuhaid-Ur-Rehman@users.noreply.github.com>
2024-12-23 11:05:27 +0500

    add repo

---
+++ main.js
@@ -0,0 +1,44 @@
+// Main Process
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  // Browser Window <- Renderer Process
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+      // // will sanitize JS code
+      // // TODO: explain when React application is initialize
+      // worldSafeExecuteJavaScript: true,
+      // // is a feature that ensures that both, your preload scripts and Electron
+      // // internal logic run in sparate context
+      // contextIsolation: true
+    }
+  })
+  win.loadFile('index.html')
+  //win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
+
+
+// Chromium -> web eingine for rendering the UI, full Chrome-like web browser
+// V8 -> engine that provides capabilities to execute, run, JS code in the browser
+// Node JS(V8) -> we are able to run JS code + provides more features
+
+// Webpack -> is a module builder, main purpose is to bundle JS files for usage in the browsert
+// Babel -> js a JS compiler
